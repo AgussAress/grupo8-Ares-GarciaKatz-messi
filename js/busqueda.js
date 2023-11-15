6ce59d6c9619e2cdbf6c69877d2c2a45421e2c6b
AgussAress <aares@udesa.edu.ar>
2023-11-15 01:17:12 -0300

    intentando el buscador

--- js/busqueda.js
+++ js/busqueda.js
@@ -1,1 +1,7 @@
 let acaVaLaAPIKey = "e085a8d4a0502afc1d3c8e65c53af130";
+let busqueda = document.querySelector('#buscador');
+let url = `https://api.themoviedb.org/3/search/movie?api_key=${acaVaLaAPIKey}&query=${busqueda}`;
+
+console.log(document.addEventListener('submit', function(){
+    console.log(url)
+}));
