a56aa5cdfa91ebc7d62e281afccd5b92cf61804d
Sebagk <garciakatzs@udesa.edu.ar>
2023-11-15 15:35:50 -0300

    empezando el buscador

--- js/busqueda.js
+++ js/busqueda.js
@@ -1,7 +1,23 @@
 let acaVaLaAPIKey = "e085a8d4a0502afc1d3c8e65c53af130";
-let busqueda = document.querySelector('#buscador');
-let url = `https://api.themoviedb.org/3/search/movie?api_key=${acaVaLaAPIKey}&query=${busqueda}`;
+let qs = location.search;
+let qsObj = new URLSearchParams(qs);
+console.log(qsObj);
+let id_busqueda = qsObj.get('buscador');
+console.log(id_busqueda)
 
-console.log(document.addEventListener('submit', function(){
-    console.log(url)
-}));
+
+let busqueda = document.querySelector('#buscador');
+let url = `https://api.themoviedb.org/3/search/movie?api_key=${acaVaLaAPIKey}&query=${id_busqueda}`;
+
+fetch(url)
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    let miData = data.results
+    console.log(miData);
+    let contenido = " ";
+})
+.catch(function(err){
+    console.log(err);
+});
