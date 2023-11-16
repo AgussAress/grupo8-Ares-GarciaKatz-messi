6eb70dbfd20cd264069c0c9b6685d24cfd57d1d3
Sebagk <garciakatzs@udesa.edu.ar>
2023-11-16 17:39:17 -0300

    avanzando con el buscador

--- js/busqueda.js
+++ js/busqueda.js
@@ -2,11 +2,10 @@ let acaVaLaAPIKey = "e085a8d4a0502afc1d3c8e65c53af130";
 let qs = location.search;
 let qsObj = new URLSearchParams(qs);
 console.log(qsObj);
-let id_busqueda = qsObj.get('buscar');
+let id_busqueda = qsObj.get("buscador");
 console.log(id_busqueda)
 
 
-let busqueda = document.querySelector('#buscador');
 let url = `https://api.themoviedb.org/3/search/movie?api_key=${acaVaLaAPIKey}&query=${id_busqueda}`;
 
 fetch(url)
@@ -15,7 +14,19 @@ fetch(url)
 })
 .then(function(data) {
     let miData = data.results
+    console.log(miData);
+    let secBus = document.querySelector("#secbus")
     let contenido = " ";
+    for (let i = 0; i < miData.length; i++) {
+        contenido += `<article class="article-pelis">
+                    <img src="./img/star wars 8.webp${miData[i].poster_path}" alt="" class="imgPelis">
+                        <h3>${miData[i].title}</h3>
+                        <p>${miData[i].overview}</p>
+                        <a href="./pelis.html" class="asas">Ver más</a>
+                    </article>`;
+        
+    }
+    secBus.innerHTML = contenido
 })
 .catch(function(err){
     console.log(err);
